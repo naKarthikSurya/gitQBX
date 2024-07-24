a807fc94759cbac9c192b408adb988384f1af07f
naKarthikSurya <n.a.karthiksurya@gmail.com>
2024-07-24 15:29:32 +0530

    Running a Dyanmically FastAPI Application With Environmentable Varibles

--- dockerdemo/main.py
+++ dockerdemo/main.py
@@ -1,8 +1,20 @@
 # main.py
 from fastapi import FastAPI
+from dotenv import load_dotenv
+from settings import get_attribute
+
+load_dotenv()
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return "Hello World"
+
+@app.get("/value/{variable}")
+def read_variable(variable: str):
+    value = get_attribute(variable)
+    return {variable: value}
+
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="localhost", port=8000)
